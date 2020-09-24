b4a01900522da578f0d45d8735fae7643c5c7c55
John Melendez <john@Johns-MBP.domain>
2020-09-24 16:34:46 -0600

    Added lower and upper case charts to scriptjs.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 //Creating arrays for all character.
 var caseArray = [];
-var lowerCaseChart = ["a",]
+var lowerCaseChart = ["a", "b", "c", "d", "e", "f", "g"
+, "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s",
+ "t", "u", "v", "w", "x", "y", "z"];
+ var upperCaseChart = ["A", "B", "C", "D", "E", "F", "G"
+ , "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S",
+  "T", "U", "V", "W", "X", "Y", "Z"];
+  
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
