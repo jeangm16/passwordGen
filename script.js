4bcb2a7ef3d2049040b68b968e988a2389ff8b3c
John Melendez <john@Johns-MBP.domain>
2020-09-26 13:23:35 -0600

    made code more readable

--- script.js
+++ script.js
@@ -1,33 +1,27 @@
 
-
 //Creating arrays for all character.
-var caseArray = [];
-var lowerCaseChart = ["a", "b", "c", "d", "e", "f", "g"
-, "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s",
- "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseChart = ["A", "B", "C", "D", "E", "F", "G"
- , "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S",
-  "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseChart = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseChart = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var numberChart = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-var specialChart = ["!", "@", "#", "$", "%", "^", "&", "*"]
+var specialChart = ["!", "@", "#", "$", "%", "^", "&", "*"];
 var passwordText = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 var possibleCharacters = "";
 var password = "";
 
 
-function getPasswordOption(){
+function writePassword(){
   //add 8 to 128 character limit
   var password = "";
   possibleCharacters = "";
   characterAmount = prompt("How many character would you like you can have 8 to 128");
   if (characterAmount < 8){
     alert("You can pick from 8 to 128")
-    return getPasswordOption();
+    return writePassword();
   }
     else if(characterAmount > 128){
       alert("You can pick from 8 to 128")
-      return getPasswordOption();
+      return writePassword();
     }
 
   var useLowerCase = confirm("Would you like to add lowercase characters?");
@@ -54,18 +48,17 @@ function getPasswordOption(){
       possibleCharacters += lowerCaseChart;
     }
     if (useNumeric){
-      possibleCharacters += 
+      possibleCharacters += numberChart 
     }
-
-
-
-
+    if (useSpecial){
+      possibleCharacters += specialChart
+    }
 
   for (var i = 0; i < characterAmount; i++) {
     password +=
       possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
   }
-passwordText.value = password;
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
